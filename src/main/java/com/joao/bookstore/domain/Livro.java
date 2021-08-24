79236b512a3d2d3e523c1108494c47a7bd9c94a6
João Pedro <joao.ppedro430@gmail.com>
2021-08-24 20:04:42 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/joao/bookstore/domain/Livro.java
@@ -0,0 +1,86 @@
+package com.joao.bookstore.domain;
+
+import java.util.Objects;
+
+public class Livro {
+
+    private Integer id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+
+    private Categoria categoria;
+
+    public Livro() {
+        super();
+    }
+
+
+    public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nome_autor = nome_autor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNome_autor() {
+        return this.nome_autor;
+    }
+
+    public void setNome_autor(String nome_autor) {
+        this.nome_autor = nome_autor;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Livro)) {
+            return false;
+        }
+        Livro livro = (Livro) o;
+        return Objects.equals(id, livro.id) && Objects.equals(titulo, livro.titulo) && Objects.equals(nome_autor, livro.nome_autor) && Objects.equals(texto, livro.texto) && Objects.equals(categoria, livro.categoria);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titulo, nome_autor, texto, categoria);
+    }
+
+
+}
